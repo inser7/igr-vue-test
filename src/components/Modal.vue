281066a9e8141531d6b5b6a9b12d84ca0779aaaa
skoltsov@citylink.pro <skoltsov@citylink.pro>
2021-04-27 18:38:31 +0300

    test async

---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+            <button class="btn btn-primary btn-sm" @click="$emit('fetch')" :disabled="wbsLoading">
+              FetchWBS
+            </button>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button class="modal-default-button" @click="$emit('close')">
+                OK
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    wbsLoading: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
